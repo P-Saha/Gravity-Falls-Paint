97a9f59ad994f7670df775997c096696924ffc2e
Priyonto Saha <priyonto2000@gmail.com>
2019-01-17 13:26:01 -0500

    Removing

--- Paint prep/paint examples/paint project - final/comp.py
+++
@@ -1,9 +0,0 @@
-import py_compile
-from glob import glob
-
-#fout = open("make.py","w")
-for n in glob("*.py"):
-    if n != "comp.py":
-        #fout.write("import " + n[:-3]+"\n")
-        py_compile.compile(n)
-#fout.close()
